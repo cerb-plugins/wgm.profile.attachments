43bc306f7b2b0fd9a7f8a44e23a35e094dd275a8
Jeff Standen <jeff@webgroupmedia.com>
2017-01-02 17:55:36 -0800

    7.3 compatibility

--- api/App.php
+++ api/App.php
@@ -19,7 +19,7 @@ class ProfileTab_WgmTicketAttachments extends Extension_ContextProfileTab {
 		if(null == ($ticket = DAO_Ticket::get($context_id)))
 			return;
 		
-		if(!$active_worker->isGroupMember($ticket->group_id))
+		if(!Context_Ticket::isReadableByActor($ticket->id, $active_worker))
 			return;
 		
 		// Load the message IDs for this ticket
@@ -28,18 +28,14 @@ class ProfileTab_WgmTicketAttachments extends Extension_ContextProfileTab {
 		
 		// Load an attachments view
 		
-		$defaults = C4_AbstractViewModel::loadFromClass('View_AttachmentLink');
+		$defaults = C4_AbstractViewModel::loadFromClass('View_Attachment');
 		$defaults->id = 'ticket_attachments';
 		$defaults->is_ephemeral = true;
 		
-		if(null == ($view = C4_AbstractViewLoader::getView($defaults->id, $defaults))) {
+		if(null == ($view = C4_AbstractViewLoader::getView($defaults->id, $defaults)))
 			return;
-		}
 		
-		$view->addParamsRequired(array(
-			 new DevblocksSearchCriteria(SearchFields_AttachmentLink::LINK_CONTEXT, '=', CerberusContexts::CONTEXT_MESSAGE),
-			 new DevblocksSearchCriteria(SearchFields_AttachmentLink::LINK_CONTEXT_ID, 'in', array_keys($messages)),
-		), true);
+		$view->addParamsWithQuickSearch(sprintf('on.msgs:(ticket.id:%d) OR on.comments:(on.ticket:(id:%d))', $context_id, $context_id), true);
 		
 		$tpl->assign('view', $view);
 		
@@ -67,21 +63,21 @@ class WgmProfileAttachmentsZipController extends DevblocksControllerExtension {
 		$stack = $request->path;
 		array_shift($stack); // example
 		
-	    @$action = array_shift($stack) . 'Action';
+		@$action = array_shift($stack) . 'Action';
 
-	    switch($action) {
-	        case NULL:
-	            break;
-	            
-	        default:
-			    // Default action, call arg as a method suffixed with Action
+		switch($action) {
+			case NULL:
+				break;
+				
+			default:
+				// Default action, call arg as a method suffixed with Action
 				if(method_exists($this,$action)) {
 					call_user_func(array(&$this, $action));
 				}
-	            break;
-	    }
-	    
-	    exit;
+				break;
+		}
+		
+		exit;
 	}
 
 	function writeResponse(DevblocksHttpResponse $response) {
@@ -98,9 +94,12 @@ class WgmProfileAttachmentsZipController extends DevblocksControllerExtension {
 		// Security
 		if(null == ($active_worker = CerberusApplication::getActiveWorker()))
 			DevblocksPlatform::dieWithHttpError($translate->_('common.access_denied'), 403);
-
+		
 		// Ticket
 		if(0 != strcasecmp($context, CerberusContexts::CONTEXT_TICKET))
+			return;
+		
+		if(!Context_Attachment::isDownloadableByActor($context_id, $active_worker))
 			return;
 		
 		if(null == ($ticket = DAO_Ticket::get($context_id)))
@@ -111,8 +110,10 @@ class WgmProfileAttachmentsZipController extends DevblocksControllerExtension {
 		if(empty($messages))
 			return;
 		
-		$context = Extension_DevblocksContext::get(CerberusContexts::CONTEXT_MESSAGE);
-		$links = DAO_AttachmentLink::getByContextIds($context->id, array_keys($messages));
+		// [TODO] This should just use the View_Attachment quicksearch above
+		// [TODO] The isDownloadableByActor check would be on the entire results
+		$files = DAO_Attachment::getByContextIds(CerberusContexts::CONTEXT_MESSAGE, array_keys($messages));
+		
 		$zip_fp = DevblocksPlatform::getTempFile();
 		$zip_filename = DevblocksPlatform::getTempFileInfo($zip_fp);
 		
@@ -128,13 +129,7 @@ class WgmProfileAttachmentsZipController extends DevblocksControllerExtension {
 			$zip = new PclZip($zip_filename);
 		}
 		
-		foreach($links as $link) {
-			// Security
-			if(!$context->authorize($link->context_id, $active_worker))
-				continue;
-				
-			$file = $link->getAttachment(); /* @var $file Model_Attachment */
-			
+		foreach($files as $file) {
 			if(false === ($fp = DevblocksPlatform::getTempFile()))
 				continue;
 			
@@ -144,13 +139,14 @@ class WgmProfileAttachmentsZipController extends DevblocksControllerExtension {
 			$fp_filename = DevblocksPlatform::getTempFileInfo($fp);
 
 			if($zip instanceof ZipArchive) { /* @var $zip ZipArchive */
-				$zip->addFile($fp_filename, $file->display_name);
+				$zip->addFile($fp_filename, $file->name);
 				
 			} else { /* @var $zip PclZip */
-				$zip->add(array(
+				$zip->add(
+					array(
 						array(
  							PCLZIP_ATT_FILE_NAME => $fp_filename,
-							PCLZIP_ATT_FILE_NEW_FULL_NAME => $file->display_name,
+							PCLZIP_ATT_FILE_NEW_FULL_NAME => $file->name,
 						),
 					),
 					PCLZIP_OPT_REMOVE_ALL_PATH
